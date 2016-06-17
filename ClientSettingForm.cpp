a0265108d1141256e51a3b4493b630986519ac1f
SOSHIK <geuaest@yahoo.co.jp>
2016-06-17 17:22:13 +0900

    [バグ修正] ・以下の不具合を修正 *マップの編集において、Cool側の初期位置が開始毎に変化してしまう *サーバーからクライアントを切断する際正常に切断されず、再接続した際正常に接続されない

--- ClientSettingForm.cpp
+++ ClientSettingForm.cpp
@@ -36,9 +36,30 @@ void ClientSettingForm::Connected  (){
     this->ui->ConnectButton->setText("　切断　");
 }
 void ClientSettingForm::DisConnected(){
+    /*
+    this->client = new TCPClient(this);
     this->ui->NameLabel ->setText("不明");
     this->ui->IPLabel   ->setText("不明");
     this->ui->StateLabel->setText("未接続");
+    this->ui->ConnectButton->setText("接続開始");
+
+    */
+    disconnect(this->client,SIGNAL( Disconnected()),this,SLOT(DisConnected()));
+    //TCP待機やめ
+    dynamic_cast<TCPClient*>(this->client)->CloseSocket();
+    this->client = new TCPClient(this);
+
+    //再connectしクライアントの接続を待つ
+    connect(this->client,SIGNAL(Connected())   ,this,SLOT(Connected()));
+    connect(this->client,SIGNAL(Ready())       ,this,SLOT(SetStandby()));
+    connect(this->client,SIGNAL(Disconnected()),this,SLOT(DisConnected()));
+    this->client->Startup();
+
+    this->ui->ConnectButton->setText("接続開始");
+    this->ui->StateLabel->setText("非接続");
+    this->ui->NameLabel->setText("不明");
+    this->ui->IPLabel->setText("不明");
+    this->ui->PortSpinBox->setEnabled(true);
 
     //状態解除
     if(this->ui->ConnectButton->isChecked())this->ui->ConnectButton->toggle();
